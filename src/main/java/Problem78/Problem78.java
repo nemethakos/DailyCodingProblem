bd7fc15e4464fc364d184159c7ae2d1e1662bfaf
Ákos Németh <nemeth.akos@gmail.com>
2018-11-17 20:04:19 +0100

    Problem 78 Heap (PriorityQueue) solution added

--- src/main/java/Problem78/Problem78.java
+++ src/main/java/Problem78/Problem78.java
@@ -2,64 +2,13 @@ package Problem78;
 
 import java.util.List;
 
+import static Problem78.Node.mergeTwoLists;
+
 /**
  * Given k sorted singly linked lists, write a function to merge all the lists
  * into one sorted singly linked list.
  */
 public class Problem78 {
-
-	/**
-	 * <p>
-	 * Compares two values: a with b.
-	 * <ul>
-	 * <li>if a < b returns a negative integer
-	 * <li>if a == b returns 0
-	 * <li>if a > b returns a positive integer
-	 * </ul>
-	 * 
-	 * @param <T> the type of the values
-	 */
-	@FunctionalInterface
-	public interface Compare<T> {
-		int compare(T a, T b);
-	}
-
-	/**
-	 * Singly linked list node type
-	 * 
-	 * @param <T> the type of value
-	 */
-	public static class Node<T> {
-		T value;
-		Node<T> next;
-
-		/**
-		 * Constructor
-		 * 
-		 * @param value the value of the node
-		 */
-		public Node(T value) {
-			super();
-			this.value = value;
-		}
-
-		public String toString() {
-			StringBuilder sb = new StringBuilder();
-			Node<T> next = this;
-			boolean first = true;
-			while (next != null) {
-				if (!first) {
-					sb.append(" -> ");
-				}
-				first = false;
-				sb.append(next.value);
-				next = next.next;
-			}
-
-			return sb.toString();
-		}
-
-	}
 
 	/**
 	 * Merges the linked lists into one list
@@ -88,73 +37,6 @@ public class Problem78 {
 		return result;
 	}
 
-	private static <T> Node<T> mergeTwoLists(Node<T> list1, Node<T> list2, Compare<T> compare) {
-		// dummy head
-		Node<T> resultHead = new Node<>(null);
-		Node<T> resultTail = resultHead;
-
-		while (list1 != null && list2 != null) {
-			var compareResult = compare.compare(list1.value, list2.value);
-			// the two values are equal
-			if (compareResult == 0) {
-				var node1 = list1;
-				var node2 = list2;
-				list1 = list1.next;
-				list2 = list2.next;
-				resultTail = appendOne(node1, resultTail);
-				resultTail = appendOne(node2, resultTail);
-			}
-			// list1.value < list2.value
-			else if (compareResult < 0) {
-				var node1 = list1;
-				list1 = list1.next;
-				resultTail = appendOne(node1, resultTail);
-			// list2.value < list1.value
-			} else {
-				var node2 = list2;
-				list2 = list2.next;
-				resultTail = appendOne(node2, resultTail);
-			}
-		}
-
-		if (list1 == null) {
-			resultTail = append(list2, resultTail);
-		} else if (list2 == null) {
-			resultTail = append(list1, resultTail);
-		}
-
-		// do not include dummy head
-		return resultHead.next;
-	}
-
-	/**
-	 * Appends node to the tail
-	 * 
-	 * @param node the node to append
-	 * @param tail the tail of the list to append to
-	 */
-	private static <T> Node<T> appendOne(Node<T> node, Node<T> tail) {
-		node.next = null;
-		tail.next = node;
-
-		return node;
-	}
-
-	/**
-	 * Appends elements from head in the first list to the tail of the second list
-	 * 
-	 * @param head the head of the first list
-	 * @param tail the tail of the second list
-	 */
-	private static <T> Node<T> append(Node<T> head, Node<T> tail) {
-		if (head != null && tail != null) {
-			while (head != null) {
-				tail.next = head;
-				head = head.next;
-				tail = tail.next;
-			}
-		}
-		return tail;
-	}
+	
 
 }
--- src/test/java/Problem78/Problem78Test.java
+++ src/test/java/Problem78/Problem78Test.java
@@ -1,101 +1,29 @@
 package Problem78;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertTrue;
 
-import Problem78.Problem78.Node;
-
 class Problem78Test {
 
-	private static final int LENGTH_OF_LIST = 10;
+	static final int LENGTH_OF_LIST = 10;
 	private static final int NUMBER_OF_LISTS = 100;
-
-	Node<Integer> getList(int from, int count, int delta) {
-		int value = from;
-		Node<Integer> head = new Node<>(value);
-		var node = head;
-		for (var i = 1; i < count; i++) {
-			value += delta;
-			node.next = new Node<Integer>(value);
-			node = node.next;
-		}
-
-		return head;
-	}
-
-	List<Node<Integer>> getLists(int numLists) {
-		List<Node<Integer>> result = new ArrayList<>();
-
-		for (var i = 1; i <= numLists; i++) {
-			result.add(getList((numLists + i) * i, LENGTH_OF_LIST, i));
-		}
-
-		return result;
-	}
-
-	void putAllValueToMap(Map<Integer, Integer> map, Node<Integer> list) {
-		while (list != null) {
-			if (map.get(list.value) == null) {
-				map.put(list.value, 1);
-			} else {
-				map.put(list.value, map.get(list.value) + 1);
-			}
-			list = list.next;
-		}
-	}
-
-	boolean hasAllValues(List<Node<Integer>> lists, Node<Integer> merged) {
-		var result = false;
-
-		Map<Integer, Integer> mapOriginal = new HashMap<>();
-		for (var list : lists) {
-			putAllValueToMap(mapOriginal, list);
-		}
-
-		Map<Integer, Integer> mapMerged = new HashMap<>();
-		putAllValueToMap(mapMerged, merged);
-
-		result = mapOriginal.equals(mapMerged);
-
-		return result;
-	}
-
-	boolean isOrdered(Node<Integer> head) {
-		var result = true;
-		Node<Integer> prev = head;
-		head = head.next;
-		while (head != null) {
-			if (prev.value > head.value) {
-				result = false;
-				break;
-			}
-			prev = head;
-			head = head.next;
-		}
-		return result;
-	}
 
 	@Test
 	void testMerge() {
 
-		var lists = getLists(NUMBER_OF_LISTS);
+		var lists = Util.getLists(NUMBER_OF_LISTS, LENGTH_OF_LIST);
 		// the lists will be destroyed after the merge, so we need the same list of
 		// linked lists for checking
-		var originalLists = getLists(NUMBER_OF_LISTS);
+		var originalLists = Util.getLists(NUMBER_OF_LISTS, LENGTH_OF_LIST);
 
 		var merged = Problem78.merge(lists, (a, b) -> a.intValue() - b.intValue());
 		// check if all values in the list of linked lists are present in the merged
 		// list
-		assertTrue(hasAllValues(originalLists, merged));
+		assertTrue(Util.hasAllValues(originalLists, merged));
 
 		// check if the merged list is in order
-		assertTrue(isOrdered(merged));
+		assertTrue(Util.isOrdered(merged));
 	}
 
 }
